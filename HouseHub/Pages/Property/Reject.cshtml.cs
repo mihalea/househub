9417e105630064bb7783ede254597857fd49ef14
Mircea Mihalea <mircea@mihalea.ro>
2018-11-27 22:24:30 +0000

    Add rejection page where a reason can be inserted

---
+++ HouseHub/Pages/Property/Reject.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using HouseHub.Data;
+using HouseHub.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HouseHub.Pages.Property
+{
+    public class RejectModel : BasePageModel
+    {
+        [BindProperty] public Accommodation Accommodation { get; set; }
+
+        [BindProperty][Required]
+        public string Reason { get; set; }
+        [BindProperty]
+        public string Message { get; set; }
+
+        public RejectModel(
+            ApplicationDbContext context, 
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+
+        public void OnGet(int id)
+        {
+            Accommodation = Context.Accommodation.Find(id);
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id, String reason)
+        {
+            Accommodation = Context.Accommodation.Find(id);
+
+            if (Accommodation != null)
+            {
+                Accommodation.Approved = false;
+                Accommodation.Pending = false;
+                Accommodation.Reason = WebUtility.HtmlEncode(reason);
+
+                await Context.SaveChangesAsync();
+
+                Message = "Application has been rejected";
+                return Page();
+            }
+
+            Message = "An internal error occurred while trying to reject the application";
+            return Page();
+        }
+    }
+}
