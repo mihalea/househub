5299af465aa9d1aeb0766244c81ed118422ef1c0
Mircea Mihalea <mircea@mihalea.ro>
2018-11-28 15:20:08 +0000

    Add user management pages

---
+++ HouseHub/Pages/User/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouseHub.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseHub.Pages.User
+{
+    public class ManageModel : BasePageModel
+    {
+        [BindProperty] public IList<SelectListItem> Users { get; set; }
+
+        public ManageModel(
+            ApplicationDbContext context, 
+            IAuthorizationService authorizationService, 
+            UserManager<IdentityUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+
+        public async Task OnGetAsync()
+        {
+            Users = await UserManager.Users.Select(u => new SelectListItem {Text = u.UserName, Value = u.Id}).ToListAsync();
+        }
+
+        public IActionResult OnPost(String Users)
+        {
+            return RedirectToPage("View", new { id = Users });
+        }
+    }
+}
---
+++ HouseHub/Pages/User/View.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HouseHub.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HouseHub.Pages.User
+{
+    public class ViewModel : BasePageModel
+    {
+        [BindProperty] public IdentityUser FormUser { get; set; }
+
+        public ViewModel(
+            ApplicationDbContext context,
+            IAuthorizationService authorizationService,
+            UserManager<IdentityUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+
+        public async Task OnGetAsync(string id)
+        {
+            FormUser = await UserManager.FindByIdAsync(id);
+        }
+    }
+}
